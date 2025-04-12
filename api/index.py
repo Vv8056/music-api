1acb81d784001a61c09b81ff29e69283da05a0da
Vv8056 <118241963+Vv8056@users.noreply.github.com>
2025-04-12 12:08:10 +0530

    index.py

--- api/index.py
+++ api/index.py
@@ -60,7 +60,8 @@ def search_song():
                 "language": song.get("language"),
                 "url": song.get("url"),
                 "image": song.get("image", [{}])[-1].get("url"),  # get last/highest quality image
-                "downloadUrl": song.get("downloadUrl", [])
+                # "downloadUrl": song.get("downloadUrl", [])
+                "downloadUrl": song.get("downloadUrl", [{}])[-1].get("downloadUrl")
             })
         return jsonify(filtered_results)
     return jsonify({"error": "No data found!"}), 404
@@ -79,7 +80,8 @@ def get_song(id):
                     "language": song.get("language"),
                     "hasLyrics": song.get("hasLyrics"),
                     "image": song.get("image", []),
-                    "downloadUrl": song.get("downloadUrl", []),
+                    # "downloadUrl": song.get("downloadUrl", []),
+                    "downloadUrl": song.get("downloadUrl", [{}])[-1].get("downloadUrl"),
                     "releaseDate": song.get("releaseDate"),
                     "playCount": song.get("playCount")
                     # "artists": song.get("artists", {}).get("primary", [])
